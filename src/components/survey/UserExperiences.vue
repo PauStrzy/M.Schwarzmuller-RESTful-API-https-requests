6a4abc8b8c1b1831502524d2ec2976f3dcbb4cb3
Paulina <paustrzy@gmail.com>
2023-01-18 09:37:24 +0100

    Look into vue.js project with https requests RESTful API

---
+++ src/components/survey/UserExperiences.vue
@@ -0,0 +1,85 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="LoadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <p v-if="isLoading">Loading...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">
+        No stored experiences found. Start adding some surveys.
+      </p>
+      <ul v-else-if="!isLoading && results && results.length > 0">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  methods: {
+    LoadExperiences() {
+      this.isLoading = true;
+      this.error = null;
+
+      fetch(
+        'https://vue-http-demo-d46a6-default-rtdb.europe-west1.firebasedatabase.app/surveys.json'
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.error = 'Failed to fetch data - please try again later.';
+        });
+    },
+  },
+  mounted() {
+    this.LoadExperiences();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
